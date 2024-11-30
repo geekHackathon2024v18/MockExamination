e41f8ed2fcd9db177b7538b89d078bc706eb3760
市村 凌久 <c1103865@planet.kanazawa-it.ac.jp>
2024-11-30 09:22:36 +0900

    add MockExaminationResponse

---
+++ data/table/MockExaminationResponseTable.py
@@ -0,0 +1,16 @@
+from datetime import datetime as dt
+
+# 模擬試験の回答情報を持つテーブル
+class MockExaminationResponseTable:
+    def __init__(self,
+        mock_examination_id: int,
+        interruption: bool,
+        mock_examination_response_id: int = None,
+        created_at: dt = dt.now(),
+        updated_at: dt = dt.now()
+    ):
+        self.mock_examination_id = mock_examination_id
+        self.interruption = interruption
+        self.mock_examination_response_id = mock_examination_response_id
+        self.created_at = created_at
+        self.updated_at = updated_at
