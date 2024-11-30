6ce124a990735e6f3a25d015e843d7dbbcb221cc
市村 凌久 <c1103865@planet.kanazawa-it.ac.jp>
2024-11-30 11:58:47 +0900

    update MockExaminationResponse implements TableInterface

--- data/table/MockExaminationResponse.py
+++ data/table/MockExaminationResponse.py
@@ -1,7 +1,8 @@
 from datetime import datetime as dt
+import TableInterface
 
 # 模擬試験の回答情報を持つテーブル
-class MockExaminationResponse:
+class MockExaminationResponse(TableInterface):
     def __init__(self,
         mock_examination_id: int,
         interruption: bool,
@@ -14,3 +15,11 @@ class MockExaminationResponse:
         self.mock_examination_response_id = mock_examination_response_id
         self.created_at = created_at
         self.updated_at = updated_at
+    def getDictData(self) -> dict:
+        return {
+            "mock_examination_id": self.mock_examination_id,
+            "interruption": self.interruption,
+            "mock_examination_response_id": self.mock_examination_response_id,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
