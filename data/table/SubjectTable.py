ff6398515cfaa6df70b122b2ca39d5dfd3a5c0aa
市村 凌久 <c1103865@planet.kanazawa-it.ac.jp>
2024-11-30 09:15:13 +0900

    fix subject_id initialize

--- data/table/SubjectTable.py
+++ data/table/SubjectTable.py
@@ -6,7 +6,7 @@ class SubjectTable:
         subject_name: str,
         subject_id: int = None
     ):
-        self.subject_id = None
+        self.subject_id = subject_id
         self.subject_name = subject_name
         self.created_at = dt.now()
         self.updated_at = dt.now()
