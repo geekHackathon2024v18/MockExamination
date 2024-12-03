3ef5e256553e4760f70f2dd2acfcc874e59c8a13
市村 凌久 <c1103865@planet.kanazawa-it.ac.jp>
2024-12-03 14:19:53 +0900

    change: Subject extends Base

--- data/table/Subject.py
+++ data/table/Subject.py
@@ -1,22 +1,23 @@
 from datetime import datetime as dt
-from TableInterface import TableInterface
+from typing import List
+from typing import Optional
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from ..database.sql_alchemy_control import Base
+
 
 # 科目の情報を持つテーブル
-class Subject(TableInterface):
-    def __init__(self,
-        subject_name: str,
-        subject_id: int = None,
-        created_at: dt = dt.now(),
-        updated_at: dt = dt.now()
-    ):
-        self.subject_id = subject_id
-        self.subject_name = subject_name
-        self.created_at = created_at
-        self.updated_at = updated_at
-    def getDictData(self) -> dict:
-        return {
-            "subject_id": self.subject_id,
-            "subject_name": self.subject_name,
-            "created_at": self.created_at,
-            "updated_at": self.updated_at
-        }
+class Subject(Base):
+    __tablename__ = "subject"
+    subject_id: Mapped[int] = mapped_column(primary_key=True)
+    subject_name: Mapped[str]
+    created_at: Mapped[dt] = mapped_column(default=dt.now())
+    updated_at: Mapped[dt] = mapped_column(default=dt.now(), onupdate=dt.now())
+
+    def __repr__(self) -> str:
+        return f"Subject(subject_id={self.subject_id!r}, subject_name={self.subject_name!r}, created_at={self.created_at!r}, updated_at={self.updated_at!r})"
