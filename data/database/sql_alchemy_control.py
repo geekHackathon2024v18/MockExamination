f65f529e2f3fc9374126c0356ffbbaaa995e8512
市村 凌久 <c1103865@planet.kanazawa-it.ac.jp>
2024-12-03 16:10:36 +0900

    create db table

--- data/database/sql_alchemy_control.py
+++ data/database/sql_alchemy_control.py
@@ -6,7 +6,19 @@ from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
 from datetime import datetime
+from data.table.subject import Subject
+from data.table.mock_examination import MockExamination
+from data.table.question import Question
+from data.table.mock_examinationResponse import MockExaminationResponse
+from data.table.question_response import QuestionResponse
+from data.table.base import Base
 
-class Base(DeclarativeBase):
-    pass
+# テーブル作成の関数
+def create_table(engine):
+    Base.metadata.create_all(engine)
+
+engine = create_engine("sqlite:///app.db", echo=True, future=True)
+
+create_table(engine)
