70ce043dec2e3e0c8b50e80b9c54c08389df3476
市村 凌久 <c1103865@planet.kanazawa-it.ac.jp>
2024-12-05 20:15:57 +0900

    update: debug read method in debug_sql_control

--- debug_sql_controll.py
+++ debug_sql_controll.py
@@ -3,9 +3,5 @@ from data.table.question import Question
 
 database = SqlAlchemyControl()
 
-database.insert.mock_examination(1, "模擬試験1", [
-    Question(question_sentence="問題1", answer="回答1", question_type=1),
-    Question(question_sentence="問題2", answer="回答2", question_type=1),
-    Question(question_sentence="問題3", answer="回答3", question_type=1)
-], 60)
-
+subject_data = database.read.subject()
+[print(subject) for subject in subject_data]
