0e64aff0bdfdea7c35406201ded201c7293b9db9
市村 凌久 <c1103865@planet.kanazawa-it.ac.jp>
2024-12-05 14:55:33 +0900

    add: create 'Insert' inner class in SqlAlchemyControl

--- data/database/sql_alchemy_control.py
+++ data/database/sql_alchemy_control.py
@@ -41,22 +41,154 @@ class SqlAlchemyControl:
             if not isinstance(subject_name, str):
                 raise TypeError("subject_name must be str")
 
-# オブジェクト追加の関数
-def insert_object(engine, obj):
-    with Session(engine) as session:
-        add_obj = obj
-        session.add(add_obj)
-        session.commit()
+            with self.__session as session:
+                subject = Subject(subject_name=subject_name)
+                session.add(subject)
+                session.commit()
 
-engine = create_engine("sqlite:///data/database/app.db", echo=True, future=True)
+        def question_stack(self,
+            mock_examination_id: int,
+            question_sentence: str,
+            question_type: int,
+            answer: str
+        ) -> None:
+            # 型チェック
+            if not isinstance(mock_examination_id, int):
+                raise TypeError("mock_examination_idはint型で入れてね")
+            if not isinstance(question_sentence, str):
+                raise TypeError("question_sentenceはstr型で入れてね")
+            if not isinstance(question_type, int):
+                raise TypeError("question_typeはint型で入れてね")
+            if not isinstance(answer, str):
+                raise TypeError("answerはstr型で入れてね")
 
-# create_table(engine)
+            self.question_list.append(
+                Question(
+                    mock_examination_id=mock_examination_id,
+                    question_sentence=question_sentence,
+                    question_type=question_type,
+                    answer=answer
+                )
+            )
 
-# subjectテーブル追加の例
-# insert_object(
-#     engine = engine,
-#     obj = Subject(
-#         subject_name = "線形代数"
-#     )
-# )
+        # 模擬試験の情報を追加する関数
+        def mock_examination(self,
+            subject_id: int,
+            mock_examination_name: str,
+            time_limit: int = None,
+        ) -> None:
+            # questionをスタックしたかを確認
+            if len(self.question_list) == 0:
+                raise ValueError("insert.question_stack()か、insert.question_list_stack()で問題を入れてから実行してね")
+            # 型チェック
+            if not isinstance(subject_id, int):
+                raise TypeError("subject_idはint型で入れてね")
+            if not isinstance(mock_examination_name, str):
+                raise TypeError("mock_examination_nameはstr型で入れてね")
+            if time_limit is not None and not isinstance(time_limit, int):
+                raise TypeError("time_limitはint型で入れてね")
+            if time_limit != None and time_limit < 0:
+                    raise ValueError("time_limitは0以上で入れてね")
+            self.__insert_subject(
+                self.question_list + [
+                    MockExamination(
+                        subject_id=subject_id,
+                        mock_examination_name=mock_examination_name,
+                        time_limit=time_limit
+                    )
+                ]
+            )
+            self.question_list = []
 
+        # 問題の回答をスタックする関数
+        def question_response_stack(self,
+            mock_examination_response_id: int,
+            question_id: int,
+            answer: str
+        ):
+            # 型チェック
+            if not isinstance(mock_examination_response_id, int):
+                raise TypeError("mock_examination_response_idはint型で入れてね")
+            if not isinstance(question_id, int):
+                raise TypeError("question_idはint型で入れてね")
+            if not isinstance(answer, str):
+                raise TypeError("answerはstr型で入れてね")
+            if len(self.question_list) == 0:
+                raise ValueError("insert.question_stack()で問題を入れてから実行してね")
+            self.response_list.append(
+                QuestionResponse(
+                    mock_examination_response_id=mock_examination_response_id,
+                    question_id=question_id,
+                    response_content=answer
+                )
+            )
+
+        # 模擬試験の回答を追加する関数
+        def mock_examination_response(self,
+            mock_examination_id: int,
+            user_id: int,
+            score: int,
+            time: int
+        ):
+            # 型チェック
+            if not isinstance(mock_examination_id, int):
+                raise TypeError("mock_examination_idはint型で入れてね")
+            if not isinstance(user_id, int):
+                raise TypeError("user_idはint型で入れてね")
+            if not isinstance(score, int):
+                raise TypeError("scoreはint型で入れてね")
+            if not isinstance(time, int):
+                raise TypeError("timeはint型で入れてね")
+            if score < 0:
+                raise ValueError("scoreは0以上で入れてね")
+            if time < 0:
+                raise ValueError("timeは0以上で入れてね")
+
+            if len(self.response_list) == 0:
+                # すでに追加されている問題の数分、未回答として追加する
+                pass
+            self.__insert_subject(
+                self.response_list + [
+                    MockExaminationResponse(
+                        mock_examination_id=mock_examination_id,
+                        user_id=user_id,
+                        score=score,
+                        time=time
+                    )
+                ]
+            )
+            self.response_list = []
+
+
+    # オブジェクト取得の関数
+    def get_object(self, table_type: tableType, id: int):
+        with self.__session as session:
+            if table_type == tableType.SUBJECT:
+                return session.execute(select(Subject)).scalars().all()
+            elif table_type == tableType.MOCK_EXAMINATION:
+                return session.get(MockExamination, id)
+            elif table_type == tableType.QUESTION:
+                return session.get(Question, id)
+            elif table_type == tableType.MOCK_EXAMINATION_RESPONSE:
+                return session.get(MockExaminationResponse, id)
+            elif table_type == tableType.QUESTION_RESPONSE:
+                return session.get(QuestionResponse, id)
+            else:
+                return None
+
+    # デバッグ用の関数
+    def main(self):
+        # シンプルなセレクト
+        stmt = select(Subject)
+        for subject in self.__session.execute(stmt).scalars():
+            print(subject)
+
+
+
+# # subjectオブジェクト削除の関数
+# def delete_subject_object_by_id(engine, subject_id: int):
+#     with Session(engine) as session:
+#         session.query(Subject).\
+#             filter(Subject.subject_id==subject_id).\
+#             delete()
+#         session.commit()
