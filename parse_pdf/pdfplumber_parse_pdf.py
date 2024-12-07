d18614849b322c535ba15c5f77596997c1b70bc9
市村 凌久 <c1103865@planet.kanazawa-it.ac.jp>
2024-12-07 15:20:26 +0900

    update: pdfplumber state

--- parse_pdf/pdfplumber_parse_pdf.py
+++ parse_pdf/pdfplumber_parse_pdf.py
@@ -4,11 +4,8 @@ import pdfplumber
 with pdfplumber.open("parse_pdf/oop_w09.pdf") as pdf:
     # テキストを抽出する
     # print(f'{pdf.pages[num_page].extract_text()}')
+    s = ""
     for pages in pdf.pages:
-        print(pages.extract_text())
-
-
-    # 表を抽出する
-    num_page = 4
-    tables = pdf.pages[num_page].find_tables()
-    print(tables[0].extract())
+        s += pages.extract_text() + "\n\n"
+        # print(pages.extract_text())
+    print(s)
